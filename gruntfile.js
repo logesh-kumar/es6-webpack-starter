28695a28e2b6c608ed35539b2870bccd811d92a5
Logesh Kumar <logeshkumar.r@krds.fr>
2016-11-18 12:55:08 +0530

    a sample grunt file

---
+++ gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      browserify: {
+        files: ['src//*.js'],
+        tasks: ['browserify']
+      }
+    },
+
+    browserify: {
+      dist: {
+        options: {
+           transform: [['babelify', {presets: ['es2015', 'react']}]]
+        },
+        src: ['src/app.js'],
+        dest: 'bundle.js',
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.registerTask('default', ['browserify']);
+
+};
